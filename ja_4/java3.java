3314836521baae08174da24994c827a4c81421af
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-06 01:15:13 +0900

    [Imprpove] add java3 in ja_4

---
+++ ja_4/java3.java
@@ -0,0 +1,10 @@
+public class java3 {
+	public static void main(String args[]) {
+		String str = "ほぷしぃ";
+		char[] arr = {'a', 'b', 'c'};
+		System.out.println("string = " + str);
+		for(int i = 0; i < 3; i++) {
+			System.out.println("array[" + i + "] = " + arr[i]);
+		}
+	}
+}
