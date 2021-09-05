8d668078bcada6f3d69198682ff71fd534ade2cd
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-06 01:09:36 +0900

    [Improve] add java2.java in ja_4

---
+++ ja_4/java2.java
@@ -0,0 +1,10 @@
+public class java2 {
+	public static void main(String args[]) {
+		char cd = 'A';
+		int id = 100;
+		double dd = 200.2;
+		System.out.println("cd = " + cd);
+		System.out.println("id = " + id);
+		System.out.println("dd = " + dd);
+	}
+}
