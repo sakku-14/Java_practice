c4533c641b487593851b2f5ed22d2b4b1aa30ef7
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-06 01:19:25 +0900

    [Improve] add java4 in ja_4

---
+++ ja_4/java4.java
@@ -0,0 +1,11 @@
+public class java4 {
+	public static void main(String args[]) {
+		int a = 30, b = 20;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + ((double)a / b));
+	}
+}
