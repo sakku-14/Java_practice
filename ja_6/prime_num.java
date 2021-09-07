9a30cd26565184a6491c46d56dcc407254f1dab9
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-07 23:08:06 +0900

    [Improve] add prime_num

--- ja_6/prime_num.java
+++ ja_6/prime_num.java
@@ -1,1 +1,20 @@
+public class prime_num {
+	public static void main(String args[]) {
+		int i = 0;
+		int tmp;
 
+		while (i <= 100)
+		{
+			tmp = i - 1;
+			while (tmp > 1)
+			{
+				if (i % tmp == 0)
+					break;
+				tmp--;
+			}
+			if (tmp == 1)
+				System.out.print(i + " ");
+			i++;
+		}
+	}
+}
