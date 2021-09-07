165271eeb63da048f14c4d7f7b2b6570fb5c9c4d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-08 00:56:15 +0900

    [Improve] triangle

---
+++ ja_6/triangle.java
@@ -0,0 +1,7 @@
+public class triangle {
+	public static void main (String args[]) {
+		System.out.println(((double)(3 * 4) / 2) + "(cm^2)");
+		System.out.println(((double)(10 * 10) / 2) + "(cm^2)");
+		System.out.println(((double)(2.5 * 8.5) / 2) + "(cm^2)");
+	}
+}
