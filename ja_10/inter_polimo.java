62ba359d860b4503acc959d2c6825d0bc861155d
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-10 21:19:45 +0900

    [Imrpove] add inter_polimo

---
+++ ja_10/inter_polimo.java
@@ -0,0 +1,27 @@
+public class inter_polimo {
+	public static void main(String args[]) {
+		Police police[] = new Police[2];
+		police[0] = new Policeman();
+		police[1] = new Policedog();
+
+		for (int i = 0; i < police.length; i++){
+			police[i].investigate();
+		}
+	}
+}
+
+interface Police {
+	void investigate();
+}
+
+class Policeman implements Police {
+	public void investigate() {
+		System.out.println("人間は聞き込みをする");
+	}
+}
+
+class Policedog implements Police {
+	public void investigate() {
+		System.out.println("犬は匂いを辿る");
+	}
+}
