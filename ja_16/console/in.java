bf4db9e7282df3d86a346688dc0b742ba69deb90
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-12 14:20:00 +0900

    [Improve] add io

---
+++ ja_16/console/in.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class in {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("値を入力してください:");
+            String s = br.readLine();
+            System.out.println("入力された値:" + s);
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
