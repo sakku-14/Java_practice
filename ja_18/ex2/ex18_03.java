019e508c2f1b679a06c7f5a701b17aa04a19f3f8
Yuki Sakuma <kumasan.04.24.14@gmail.com>
2021-09-14 14:48:38 +0900

    [Improve] io example

--- ja_18/ex2/ex18_03.java
+++ ja_18/ex2/ex18_03.java
@@ -1,26 +1,56 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ex18_03 {
     public static void main(String[] args) {
-        
+        try {
+            BufferedReader br1 = new BufferedReader(new FileReader("in1.txt"));
+            BufferedReader br2 = new BufferedReader(new FileReader("in2.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("out.txt"));
+
+            FileInOut f1 = new FileInOut(br1, bw);
+            FileInOut f2 = new FileInOut(br2, bw);
+
+            f1.start();
+            f2.start();
+
+            f1.join();
+            f2.join();
+
+            br1.close();
+            br2.close();
+            bw.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
-public class FileInOut extends Thread {
+class FileInOut extends Thread {
     private BufferedReader br;
-    private String str[];
+    private BufferedWriter bw;
     
-    public FileInOut (String file) {
-        br = new BufferedReader(new FileReader(file));
-        str[] = new String[];
+    public FileInOut (BufferedReader br, BufferedWriter bw) {
+        this.br = br;
+        this.bw = bw;
     }
 
-    public synchronized run() {
-        int i = 0;
-        while (true) {
-            
-            br.readLine()
+    public synchronized void run() {
+        try {
+            String str;
+        
+            while (true) {
+                str = br.readLine();
+                if (str == null) break;
+                bw.write(str);
+            }
+        } catch (Exception e) {
+            System.err.println(e);;
         }
     }
 }
